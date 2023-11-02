2832d4732e202d8b1c282ef3a4fd0249feab1fb5
James Jackson <james@pop-os.localdomain>
2023-11-02 13:46:14 -0500

    Optimized union-find algorithm.
    Got rid of tree datatype, since all we need are parent pointers.
    Added path compression to the find method.

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ mod union_find;
 
 use rand::Rng;
 
-use union_find::{Forest, Tree};
 use std::collections::HashSet;
+use union_find::Forest;
 
 // A maze consisting of square cells numbered from 0 to WIDTH - 1 and
 // 0 to HEIGHT - 1.
@@ -16,10 +16,12 @@ pub struct Maze {
 
 impl Maze {
     pub fn new(width: usize, height: usize) -> Maze {
-	Maze {
-	    width, height, edges: HashSet::new()
-	    }
-	}
+        Maze {
+            width,
+            height,
+            edges: HashSet::new(),
+        }
+    }
     pub fn has_edge(&self, p1: Point, p2: Point) -> bool {
         self.edges.contains(&Edge::new(p1, p2)) || self.edges.contains(&Edge::new(p2, p1))
     }
@@ -96,20 +98,15 @@ impl Maze {
         println!("+");
     }
     pub fn add_walls(&mut self) {
-        let mut forest = Forest {
-            count: (self.width - self.height) as usize,
-            forest: vec![],
-        };
+        let mut forest = Forest::new();
         let mut edge_set = vec![];
         let width = self.width;
         let height = self.height;
+        let mut distinct = width * height;
         for j in 0..self.height {
             for i in 0..self.width {
                 let tree_num = (width * j + i) as usize;
-                forest.forest.push(Tree {
-                    id: tree_num,
-                    parent: tree_num,
-                });
+                forest.push(tree_num);
                 if i < width - 1 && j < height - 1 {
                     edge_set.push(Edge::new((i, j), (i + 1, j)));
                     edge_set.push(Edge::new((i, j), (i, j + 1)));
@@ -120,19 +117,19 @@ impl Maze {
                 }
             }
         }
-	self.edges = edge_set.clone().into_iter().collect();
         let mut rng = rand::thread_rng();
-        while forest.distinct_sets() > 1 {
-            let edge = edge_set.swap_remove(rng.gen_range(0..edge_set.len()));
-            let p1 = edge.p1.0 + width * edge.p1.1;
-            let p2 = edge.p2.0 + width * edge.p2.1;
-            assert!(forest.forest[p1].id == p1);
-            assert!(forest.forest[p2].id == p2);
-            if forest.walk(p1) != forest.walk(p2) {
-                self.edges.remove(&edge);
+        while distinct > 1 {
+            let edge_index = rng.gen_range(0..edge_set.len());
+            let edge = &edge_set[edge_index];
+            let p1 = edge.p1.1 + width * edge.p1.0;
+            let p2 = edge.p2.1 + width * edge.p2.0;
+            if forest.find(p1) != forest.find(p2) {
+                edge_set.remove(edge_index);
                 forest.union(p1, p2);
+                distinct -= 1;
             }
         }
+        self.edges = edge_set.clone().into_iter().collect();
     }
 }
 
--- src/union_find.rs
+++ src/union_find.rs
@@ -2,37 +2,37 @@
 // parent attributes of the Tree struct refer to a tree number.
 #[derive(Debug)]
 pub struct Forest {
-    pub count: usize,
-    pub forest: Vec<Tree>,
-}
-
-#[derive(Debug)]
-pub struct Tree {
-    pub id: usize,
-    pub parent: usize,
+    pub parents: Vec<usize>,
 }
 
 impl Forest {
-    pub fn walk(&self, tree: usize) -> usize {
-        let t = &self.forest[tree];
-        assert!(t.id == tree);
-        if t.id == t.parent {
-            t.id
+    pub fn new() -> Forest {
+        Forest { parents: vec![] }
+    }
+    pub fn push(&mut self, new: usize) {
+        assert!(new == self.parents.len());
+        self.parents.push(new);
+    }
+    pub fn find(&mut self, i: usize) -> usize {
+        if self.parents[i] == i {
+            i
         } else {
-            self.walk(t.parent)
+            let found = self.find(self.parents[i]);
+            self.parents[i] = found;
+            found
         }
     }
     pub fn union(&mut self, t1: usize, t2: usize) {
-        let root1 = self.walk(t1);
-        let root2 = self.walk(t2);
-        if root1 < root2 {
-            self.forest[root2].parent = root1;
+        let root1 = self.find(t1);
+        let root2 = self.find(t2);
+        if root2 < root1 {
+            self.parents[root1] = root2;
         } else {
-            self.forest[root1].parent = root2;
+            self.parents[root2] = root1;
         }
     }
-    pub fn distinct_sets(&self) -> usize {
-        let mut parents: Vec<usize> = self.forest.iter().map(|t| self.walk(t.id)).collect();
+    pub fn distinct_sets(&mut self) -> usize {
+        let mut parents: Vec<usize> = self.parents.clone().iter().map(|p| self.find(*p)).collect();
         parents.sort();
         parents.dedup();
         parents.len()
